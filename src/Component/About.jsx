9604668c0cec9b46b0fb832b58b6eece5aeb5417
james <jaydhales@gmail.com>
2022-02-02 08:47:58 +0100

    Favicon and Meta

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,7 +1,7 @@
 export default function About({ toContact }) {
   return (
     <main
-      className=' mt-12 grid md:place-items-center my-1 gap-4 h-auto'
+      className=' mt-12 grid md:place-items-center my-1 gap-4 h-[700px]'
       id='home'
     >
       <div className='w-[250px] h-[250px] rounded-full bg-[#41f2ff] bg-opacity-20 relative m-auto'>
